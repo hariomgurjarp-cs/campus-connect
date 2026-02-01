f66a7dc2761d03b7ec7b2c6a681be1bd8f6c0f8f
Hariom gurjar Patel <hariomgurjarp@gmail.com>
2026-02-01 08:51:30 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-4000936816-c6d85",
+  "appId": "1:1026495455956:web:28b8a93cc9f72eafc3d141",
+  "apiKey": "AIzaSyD60wOlbthl3Rmxyqg9EY89G5nXcrILyvs",
+  "authDomain": "studio-4000936816-c6d85.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "1026495455956"
+};
